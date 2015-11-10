1b407ef62578f7c516fe34caaba44bbf2e2a21ea
gdmec07140546 <1101517784@qq.com>
2015-11-10 12:38:43 +0800

    Initial commit

---
+++ app/src/main/java/com/example/administrator/work_4/MyDB.java
@@ -0,0 +1,139 @@
+package com.example.administrator.work_4;
+
+/**
+ * Created by lin on 2015/11/4.
+ */
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+
+public class MyDB extends SQLiteOpenHelper {
+    private static String DB_NAME="My_DB.db";
+    private static int DB_VERSION=2;
+    private SQLiteDatabase db;
+    //构造方法
+    public MyDB(Context context) {
+        super(context,DB_NAME, null, DB_VERSION);
+        db=getWritableDatabase();
+    }
+    //链接数据库
+    public SQLiteDatabase openConnection(){
+        if(!db.isOpen())
+        {
+            //读写方式获取SQLiteDatabase
+            db=getWritableDatabase();
+        }
+        return db;
+
+    }
+    //光闭数据库链接
+    public void closeConnection(){
+        try {
+            if (db!=null&&db.isOpen())
+                db.close();
+
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+    //创建数据表
+    public boolean createTable(String createTableSql){
+        try {
+            openConnection();
+            db.execSQL(createTableSql);
+        }catch(Exception e) {
+            e.printStackTrace();
+            return false;
+        }finally {
+            closeConnection();
+        }
+        return true;
+    }
+    //保存数据
+    public boolean save(String tableName,ContentValues values){
+        try {
+            openConnection();
+            db.insert(tableName, null, values);
+        }catch(Exception e) {
+            e.printStackTrace();
+
+        }finally {
+            closeConnection();
+        }
+        return true;
+    }
+    //更新数据
+    public boolean update(String table,ContentValues values,String whereClause,String whereArgs[]){
+        try {
+            openConnection();
+            db.update(table, values, whereClause, whereArgs);
+        }catch(SQLException e) {
+            e.printStackTrace();
+            return false;
+
+        }finally {
+            closeConnection();
+        }
+        return true;
+    }
+    //删除数据
+    public boolean delete(String table,String deleteSql,String obj[]){
+        try {
+            openConnection();
+            db.delete(table, deleteSql, obj);
+        }catch(SQLException e) {
+            e.printStackTrace();
+            return false;
+
+        }finally {
+            closeConnection();
+        }
+        return true;
+    }
+    //查找数据
+    public Cursor find(String findSql,String obj[]){
+        try {
+            openConnection();
+            Cursor cursor=db.rawQuery(findSql,obj);
+            return cursor;
+        }catch(SQLException e) {
+            e.printStackTrace();
+            return null;
+
+        }
+    }
+    //检查数据表是否存在
+    public boolean isTableExist(String tableName){
+        try {
+            openConnection();
+            String str="select count(*)xcount from " + tableName;
+            db.rawQuery(str,null).close();
+
+        }catch(Exception e) {
+
+            return false;
+
+        }
+        return true;
+    }
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+
+    @Override
+    public void onOpen(SQLiteDatabase db) {
+        //todo  每次成功打开数据库后首先执行
+        super.onOpen(db);
+    }
+}
+
